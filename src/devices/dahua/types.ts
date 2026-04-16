c9d6e1797f375cbf03e8f2ef720f72f74805d225
Gabriel Hobold Santini <gabriel.hobolds@gmail.com>
2026-04-16 02:54:41 -0300

    feat: add time settings configuration and test coverage report

--- src/devices/dahua/types.ts
+++ src/devices/dahua/types.ts
@@ -8,3 +8,18 @@ export interface InvasionAreaPoint {
 export type ImageQualityConfiguration = BaseImageQualityConfiguration & {
     shotQuality?: 'minimum' | 'medium' | 'maximum';
 };
+
+export type TimeConfiguration = {
+    timeFormat?: string; // e.g: dd-MM-yyyy HH:mm:ss
+    ntp?: {
+        enabled: boolean;
+        server?: string;
+        port?: number;
+        interval?: number; // in minutes 0 to 1440 (0 = no calibration)
+    };
+    timezoneName?: string; // e.g: "UTC-0" or "Brasilia"
+    timeZoneId?: number; // e.g: 0 for UTC-0, 22 for Brasilia
+    dst?: {
+        enabled: boolean;
+    }
+};
